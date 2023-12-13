644c133babf85bde14a5bac0f255fe0d1cb70cf5
jlzang <jlzang99@gmail.com>
2023-12-12 20:59:06 -0600

    Add event listener to button to prompt user for new grid size
    Event is exited if the input is NaN, less than 0, or greater than 100.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 16; i++) {
@@ -33,4 +34,21 @@ function randomHsl() {
     let s = randomSaturation(50, 80);
     let l = randomLightness(40, 60);
     return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    let newGrid = Number(prompt("Please enter a number 1-100."));
+    if (isNaN(newGrid) || newGrid < 1 || newGrid > 100)
+    alert("Invalid input, please try again!")
+})
+
+function createGridRow(num) {
+    for (let i = 0; i < num; i++) {
+
+    }
+}
+function createNewGrid(num) {
+
 }
